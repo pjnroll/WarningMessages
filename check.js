f0599f8582de6b650b5bc6b54d7971d21a696255
pjnroll <pjlaviano35@gmail.com>
2020-03-21 19:26:16 +0100

    Added method to check maximum 3 indicators chosen (but id doesn't work)

--- check.js
+++ check.js
@@ -11,11 +11,24 @@ function getCheckedBoxes(formName) {
 }
 
 
-function checkForm() {
-	var checkedBoxes = getCheckedBoxes("warn_form");
+function checkFormDynamic() {
+	var checkedBoxes = getCheckedBoxes("warn_form_dynamic");
 	if (checkedBoxes===null){
     	alert("You didn't check any indicator!");
     } else {
-		document.getElementById("warn_form").submit();
+      alert("Checked->" + checkedBoxes.length);
+		document.getElementById("warn_form_dynamic").submit();
 	}
+}
+
+function prova(id) {
+  var checkedBoxes = getCheckedBoxes("warn_form_manual");
+  alert("Checked->"+checkedBoxes.length);
+  if (checkedBoxes.length > 3) {
+    alert("You cannot select more indicators (maximum 3)");
+    document.getElementById(id).checked = false;
+  } else {
+    alert("We" + id);
+  }
+
 }
