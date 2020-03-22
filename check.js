f999ac74d8be2f116de04f617ee6106d8a43273f
pjnroll <pjlaviano35@gmail.com>
2020-03-22 17:31:26 +0100

    Added methods to handle errors in choosing indicators and messages

--- check.js
+++ check.js
@@ -10,19 +10,6 @@ function getCheckedBoxes(formName) {
   return checkboxesChecked.length > 0 ? checkboxesChecked : null;
 }
 
-function getCheckedRadio(formName) {
-  var radios = document.getElementById(formName);
-  var radiosChecked = [];
-
-  for (var i=0; i<radios.length; i++) {
-     if (radios[i].type=="radio" && radios[i].checked) {
-        radiosChecked.push(radios[i].value);
-     }
-  }
-  return radiosChecked.length > 0 ? radiosChecked : null;
-}
-
-
 function checkFormDynamic() {
 	var checkedBoxes = getCheckedBoxes("warn_form_dynamic");
 	if (checkedBoxes===null){
@@ -32,46 +19,84 @@ function checkFormDynamic() {
 	}
 }
 
-function readForm() {
-  var method = "manual";
-  var checkedBoxes = getCheckedBoxes("warn_form_manual");
-  var indicators = [];
-  var messages = [];
-  for (var i=0; i < checkedBoxes.length; i++) {
-    //alert("indicators:"+checkedBoxes[i]);
-    if (i % 2 == 0) {
-      indicators.push(checkedBoxes[i]);
-    } else {
-      messages.push(checkedBoxes[i]);
+function validIndicators(indicators) {
+  for (var i = 0; i < indicators.length; i++) {
+    //alert("Indicators->"+indicators[i]);
+    if (!isValidIndicator(indicators[i])) {
+      return false;
     }
   }
-  //var checkedRadio = getCheckedRadio("warn_form_manual");
-  var json_params = {"method":method, "indicators":indicators, "messages":messages};
-  location.replace("/warning_page.php" + "?" + $.param(json_params));
-
+  return true;
 }
 
-function checkFormManual(ident) {
-	var checkedBoxes = getCheckedBoxes("warn_form_dynamic");
-	if (checkedBoxes===null){
-    	alert("You didn't check any indicator!");
+function isValidIndicator(indicator) {
+  if (indicator >= 0 && indicator <= 4) {
+    return false;
+  }
+  return true;
+}
+
+function validMessages(messages) {
+  for (var i = 0; i < messages.length; i++) {
+    //alert("Messages->"+messages[i]);
+    if (!isValidMessage(messages[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function isValidMessage(message) {
+  if (message >= 0 && message <= 4) {
+    return true;
+  }
+  return false;
+}
+
+function checkFormManual(indicators, messages) {
+  if (!validIndicators(indicators) || !validMessages(messages)) {
+    return "You made mistakes in choosing indicators and messages. Check better.";
+  } else if (indicators.length != messages.length) {
+    return "You didn't chose one ore more messages";
+  } else if (indicators.length > 3) {
+    return "You selected more than 3 indicators. 3 maximum allowed.";
+  }
+
+  return "true";
+}
+
+function readForm() {
+  var method = "manual";
+  var checkedBoxesRadios = getCheckedBoxes("warn_form_manual");
+  var indicators = [];
+  var messages = [];
+  for (var i=0; i < checkedBoxesRadios.length; i++) {
+    // it gets 1 indicator and 1 message, alternating each time
+    if (i % 2 == 0) {
+      indicators.push(checkedBoxesRadios[i]);
     } else {
-		document.getElementById("warn_form_dynamic").submit();
-	}
-
-  var checkedBoxes = getCheckedBoxes("warn_form_manual");
-  for (var i = 0; i < checkedBoxes.length; i++) {
-    alert("chk " + checkedBoxes[i].value + " = " + checkedBoxes[i].checked);
+      messages.push(checkedBoxesRadios[i]);
+    }
   }
-  //alert("We" + id);
-  //alert("Checked->"+checkedBoxes.length);
-  if (checkedBoxes.length > 3) {
-    document.getElementById(id).checked = false;
-    alert("You cannot select more indicators (maximum 3)");
+  var ans = checkFormManual(indicators, messages);
+  if (ans != "true") {
+    alert(ans);
+  } else {
+    var json_params = {"method":method, "indicators":indicators, "messages":messages};
+    location.replace("/warning_page.php" + "?" + $.param(json_params));
   }
-    alert("dopo chk " + ident + ":" + ident.checked);
 }
 
-function formManualSubmit() {
-  document.getElementById("warn_form_manual").submit();
+function checkCheckbox(ident) {
+  /*var form = document.getElementById("warn_form_manual").innerHTML;
+  alert("form\n"+form);*/
+  var el = document.getElementById("chk_" + ident);
+  alert(el.type);
+  alert(ident + "->" + el.checked);
+  if (el.checked == false) {
+    var radio = document.getElementById("mess_" + ident);
+    radio.checked = true;
+    radio.checked = false;
+  }
+
 }
